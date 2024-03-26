b8da7b9d25417a3cc8dd0819d1222a7d66ee045f
Jinay Vora <jinay.vora@tesco.com>
2024-03-26 12:38:33 +0530

    Add subscription & management group mapping

--- modules/subscriptionVending/subscription.tf
+++ modules/subscriptionVending/subscription.tf
@@ -1,13 +1,18 @@
-
-
-data "azurerm_billing_enrollment_account_scope" "example" {
+data "azurerm_billing_enrollment_account_scope" "billing_scope" {
   billing_account_name    = var.billing_account_name
   enrollment_account_name = var.enrollment_account_name
 }
 
+data "azurerm_management_group" "mgmt_group" {
+  name = var.azure_mgmt_group
+}
 
-
-resource "azurerm_subscription" "example" {
+resource "azurerm_subscription" "subscription" {
   subscription_name = var.subscription_name
-  billing_scope_id  = data.azurerm_billing_enrollment_account_scope.example.id
+  billing_scope_id  = data.azurerm_billing_enrollment_account_scope.billing_scope.id
+}
+
+resource "azurerm_management_group_subscription_association" "subscription_mgmtgrp_assoc" {
+  management_group_id = data.azurerm_management_group.mgmt_group.id
+  subscription_id     = azurerm_subscription.subscription.id
 }
--- modules/subscriptionVending/variables.tf
+++ modules/subscriptionVending/variables.tf
@@ -1,8 +1,20 @@
 
 variable "subscription_name" {
+  description = "(Required) The Name of the Subscription. This is the Display Name in the portal."
+  type        = string
 }
 
 variable "enrollment_account_name" {
+  description = "(Required) Name of the Enrollment Account"
+  type        = string
 }
+
 variable "billing_account_name" {
+  description = "(Required) Name of the billing account"
+  type        = string
+}
+
+variable "azure_mgmt_group" {
+  description = "(Required) Name of the Management Group where subscription needs to be created"
+  type        = string
 }
