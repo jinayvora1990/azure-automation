e8b59086b2a7f5bc14068119cccb972b46d4a253
sudhanshujain <sudhanshu1691@gmail.com>
2024-04-04 10:59:43 +0530

    updated module for log analytics.

--- stacks/avd/variables.tf
+++ stacks/avd/variables.tf
@@ -17,36 +17,7 @@ variable "storage_accounts" {
   default = []
 }
 
-variable "subnets_for_vnet" {
-  description = "List of subnets to be created"
-  type        = map(object(
-    {
-    name                            = string
-    address_prefixes                = list(string)
-    private_endpoint_network_policies_enabled = bool
-  }
-  ))
-  default = {}
-}
 
-variable "peering_to_hub_name" {
-  default = "peer-avd-to-Hub"
-}
-
-variable "nsg_name" {
-  default = "nsg-snet-avd-prod-uaenorth-001"
-}
-#
-#variable "resource_groups" {
-#  description = "Map of resource group names and their locations"
-#  type        = map(string)
-#  default = {
-#    "shared"   = "rg-shared-avd-prod-uaenorth-01"
-#    "storage"  = "rg-storage-avd-prod-uaenorth-01"
-#    "avd"      = "rg-vnet-avd-prod-uaenorth-01"
-#    "akv"      = "rg-akv-avd-prod-uaenorth-01"
-#  }
-#}
 
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
