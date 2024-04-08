a6ed458c3f762705ad4bf66f13c84c2dd97816c2
NamanKaushik <naman.kaushik@thoughtworks.com>
2024-04-08 13:29:35 +0530

    Correction for the backup vault after rebase

--- modules/backup/revocery-services-vault/data.tf
+++ modules/backup/revocery-services-vault/data.tf
@@ -11,6 +11,6 @@ data "azurerm_private_endpoint_connection" "pep_connection" {
 }
 
 data "azurerm_key_vault" "key_vault" {
-  name                = var.encryption_config.encryption_key.vault.vault_name
+  name                = var.encryption_config.encryption_key.vault.key_vault_name
   resource_group_name = var.encryption_config.encryption_key.vault.resource_group_name
 }
