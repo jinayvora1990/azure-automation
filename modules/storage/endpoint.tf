5d6b2a89df848c82df180e90a67175cf106f67ed
NamanKaushik <naman.kaushik@thoughtworks.com>
2024-04-24 14:41:52 +0530

    Made the correction for storage module private endpoints

--- modules/storage/endpoint.tf
+++ modules/storage/endpoint.tf
@@ -1,9 +1,9 @@
 resource "azurerm_private_endpoint" "file_sa" {
-  count               = length(var.file_shares) > 0 ? 1 : 0
+  count               = length(var.file_shares) > 0 && var.privatelink_subnet != null ? 1 : 0
   name                = format("pep-sa-file-%s-%s-%s", var.application_name, local.environment, local.region_shortcode)
   location            = var.location
   resource_group_name = var.resource_group
-  subnet_id           = data.azurerm_subnet.privatelink_subnet.id
+  subnet_id           = data.azurerm_subnet.privatelink_subnet.0.id
 
   private_service_connection {
     name                           = format("%s%s", azurerm_storage_account.storeacc.name, "-sa-file-privatelink")
@@ -14,11 +14,11 @@ resource "azurerm_private_endpoint" "file_sa" {
 }
 
 resource "azurerm_private_endpoint" "blob_sa" {
-  count               = length(var.containers_list) > 0 ? 1 : 0
+  count               = length(var.containers_list) > 0 && var.privatelink_subnet != null ? 1 : 0
   name                = format("pep-sa-file-%s-%s-%s", var.application_name, local.environment, local.region_shortcode)
   location            = var.location
   resource_group_name = var.resource_group
-  subnet_id           = data.azurerm_subnet.privatelink_subnet.id
+  subnet_id           = data.azurerm_subnet.privatelink_subnet.0.id
 
   private_service_connection {
     name                           = format("%s%s", azurerm_storage_account.storeacc.name, "-sa-blob-privatelink")
@@ -29,11 +29,11 @@ resource "azurerm_private_endpoint" "blob_sa" {
 }
 
 resource "azurerm_private_endpoint" "table_sa" {
-  count               = length(var.tables) > 0 ? 1 : 0
+  count               = length(var.tables) > 0 && var.privatelink_subnet != null ? 1 : 0
   name                = format("pep-sa-file-%s-%s-%s", var.application_name, local.environment, local.region_shortcode)
   location            = var.location
   resource_group_name = var.resource_group
-  subnet_id           = data.azurerm_subnet.privatelink_subnet.id
+  subnet_id           = data.azurerm_subnet.privatelink_subnet.0.id
 
   private_service_connection {
     name                           = format("%s%s", azurerm_storage_account.storeacc.name, "-sa-table-privatelink")
@@ -42,19 +42,3 @@ resource "azurerm_private_endpoint" "table_sa" {
     subresource_names              = ["Table"]
   }
 }
-
-resource "azurerm_private_endpoint" "queue_sa" {
-  count               = length(var.queues) > 0 ? 1 : 0
-  name                = format("pep-sa-file-%s-%s-%s", var.application_name, local.environment, local.region_shortcode)
-  location            = var.location
-  resource_group_name = var.resource_group
-  subnet_id           = data.azurerm_subnet.privatelink_subnet.id
-
-  private_service_connection {
-    name                           = format("%s%s", azurerm_storage_account.storeacc.name, "-sa-queue-privatelink")
-    private_connection_resource_id = azurerm_storage_account.storeacc.id
-    is_manual_connection           = false
-    subresource_names              = ["Queue"]
-  }
-}
-
