b7b04cf8347ce9b4152854a8a109ef46d7a82c31
sudhanshujain <sudhanshu1691@gmail.com>
2024-05-07 21:11:55 +0530

    added policy module.

--- modules/policy/rg.tf
+++ modules/policy/rg.tf
@@ -10,23 +10,19 @@ resource "azurerm_policy_definition" "enforce_region_policy" {
     }
   METADATA
 
-  policy_rule = <<POLICY_RULE
-    {
-      "if": {
-        "not": {
-          "field": "location",
-          "equals": "uaenorth"
-        }
-      },
-      "then": {
-        "effect": "deny"
-      }
-    }
-  POLICY_RULE
+  policy_rule = file("${path.module}/policies/resource-location/policy-rule.json")
+  parameters  = file("${path.module}/policies/resource-location/policy-parameters.json")
 }
 
 resource "azurerm_resource_group_policy_assignment" "enforce_region_assignment" {
   name                 = "enforceRegionAssignment"
   resource_group_id    = "/subscriptions/e1811a95-7c51-4955-a86e-de0ce0c2cf73/resourceGroups/aks-test-rg"
   policy_definition_id = azurerm_policy_definition.enforce_region_policy.id
+  parameters           = <<PARAMETERS
+    {
+      "allowedLocations": {
+        "value": ["uaenorth"]
+      }
+    }
+  PARAMETERS
 }
