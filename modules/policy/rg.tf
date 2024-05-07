8337e8fa57ebc7327cbde5ad4214fb1c2ee1d5b5
sudhanshujain <sudhanshu1691@gmail.com>
2024-05-07 16:35:05 +0530

    added policy module.

---
+++ modules/policy/rg.tf
@@ -0,0 +1,32 @@
+resource "azurerm_policy_definition" "enforce_region_policy" {
+  name         = "enforceRegionPolicy"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Enforce region policy definition"
+
+  metadata = <<METADATA
+    {
+      "category": "Resource Management"
+    }
+  METADATA
+
+  policy_rule = <<POLICY_RULE
+    {
+      "if": {
+        "not": {
+          "field": "location",
+          "equals": "uaenorth"
+        }
+      },
+      "then": {
+        "effect": "deny"
+      }
+    }
+  POLICY_RULE
+}
+
+resource "azurerm_resource_group_policy_assignment" "enforce_region_assignment" {
+  name                 = "enforceRegionAssignment"
+  resource_group_id    = "/subscriptions/e1811a95-7c51-4955-a86e-de0ce0c2cf73/resourceGroups/aks-test-rg"
+  policy_definition_id = azurerm_policy_definition.enforce_region_policy.id
+}
