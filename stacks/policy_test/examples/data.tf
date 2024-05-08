95b8ff2a36dc146b3935851b07fae706ddb44628
sudhanshujain <sudhanshu1691@gmail.com>
2024-05-08 17:37:52 +0530

    added policy module.

---
+++ stacks/policy_test/examples/data.tf
@@ -0,0 +1,8 @@
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
+
+
+data "azurerm_resource_group" "scope" {
+  name = "aks-test-rg"
+}
---
+++ stacks/policy_test/examples/definitions.tf
@@ -0,0 +1,14 @@
+module "whitelist_regions" {
+  source          = "../../../modules/policyNew/modules/definition"
+  policy_name     = "whitelist_regions"
+  display_name    = "Whitelist Azure Regions"
+  policy_category = "General"
+  #  management_group_id = data.azurerm_resource_group.scope.id
+}
+
+module "storageAccountSKU" {
+  source          = "../../../modules/policyNew/modules/definition"
+  policy_name     = "allowedsku"
+  display_name    = "Allowed Storage Account SKU"
+  policy_category = "storage"
+}
