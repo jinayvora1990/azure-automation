f3e0effb9d4fc362434d44f1ba49acd3238b1db7
sudhanshujain <sudhanshu1691@gmail.com>
2024-05-13 14:10:19 +0530

    added policy module variables.tf

--- stacks/policy_test/examples/variables.tf
+++ stacks/policy_test/examples/variables.tf
@@ -1,11 +1,3 @@
-#variable "policy_name" {
-#  type = map(any)
-#  default = {
-#    whitelist_regions = "Whitelist Azure Regions",
-#    allowedsku = "Allowed Storage Account SKU"
-#  }
-#}
-#
 
 variable "policy_details" {
   type = map(object({
@@ -14,12 +6,6 @@ variable "policy_details" {
     assignment_parameters = any
   }))
   default = {
-    example_policy_1 = {
-      policy_name       = "whitelist_regions"
-      assignment_effect = "Deny"
-      assignment_parameters = {
-        listOfRegionsAllowed = ["uaenorth", "uswest"]
-      }
-    }
+
   }
 }
