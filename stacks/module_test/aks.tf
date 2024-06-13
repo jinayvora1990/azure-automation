50e70564ef83233a8279cf1207005ba0c2f3dc72
sudhanshujain <sudhanshu1691@gmail.com>
2024-06-13 17:47:40 +0530

    updated aks module.

--- stacks/module_test/aks.tf
+++ stacks/module_test/aks.tf
@@ -14,31 +14,23 @@ module "aks-create" {
 
   aks_subnet = {
     #name           = module.base-infra.subnet_names[3]
-    name           = "akstest"
+    name           = module.base-infra.subnet_names[3]
     vnet_name      = module.base-infra.vnet_name
     resource_group = module.resource_group.rg_name
   }
   depends_on                                    = [module.base-infra]
+  aks_network_plugin                            = "azure"
   resource_group_name                           = module.aks_resource_group.rg_name
   environment                                   = local.environment
   application_name                              = var.application_name
   api_server_authorized_ip_ranges               = var.aks_api_server_authorized_ip_ranges
   dapr_enabled                                  = true
   azure_active_directory_admin_group_object_ids = ["77e919ac-290c-4493-b3aa-0a74aaf69ca5"]
+  oms_agent_log_analytics_workspace_id          = module.law.law_id
   tags                                          = local.tags
-  acr = [
-    {
-      name           = "akstest02"
-      resource_group = "aks-test-rg"
-    },
-    {
-      name           = "akstest03"
-      resource_group = "aks-test-rg"
+
+  acr = [{
+    name           = module.registry.acr_name
+    resource_group = module.registry.acr_rg
   }]
-
-
-  #  acr = [{
-  #    name           = module.acr.acr_name
-  #    resource_group = module.acr.acr_rg
-  #  }]
 }
