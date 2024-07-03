e4032d39bee082c7dd75aea8f3c304370db0e73e
sudhanshujain <sudhanshu1691@gmail.com>
2024-07-03 14:42:54 +0530

    updated for nsg data type changes.

--- modules/base-infrastructure/outputs.tf
+++ modules/base-infrastructure/outputs.tf
@@ -6,3 +6,14 @@ output "vnet_name" {
 output "subnet_names" {
   value = values(azurerm_subnet.snet)[*].name
 }
+
+#output "subnet_map" {
+#  value = {
+#    for snet in azurerm_subnet.snet :
+#    snet.name => {
+#      name           = snet.name
+#      vnet_name      = azurerm_virtual_network.vnet.name
+#      resource_group = var.resource_group_name
+#    }
+#  }
+#}
